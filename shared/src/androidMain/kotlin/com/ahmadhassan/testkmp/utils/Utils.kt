45deafd6c2c1193b438f73b399d02a2f94c0db5f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-04-21 12:55:24 +0600

    fixes with comments

--- shared/src/androidMain/kotlin/com/ahmadhassan/testkmp/utils/Utils.kt
+++ shared/src/androidMain/kotlin/com/ahmadhassan/testkmp/utils/Utils.kt
@@ -4,4 +4,4 @@ import androidx.compose.runtime.Composable
 import dev.icerock.moko.resources.StringResource
 
 @Composable
-actual fun stringResource(res: StringResource) = androidx.compose.ui.res.stringResource(res.resourceId)
+internal actual fun stringResource(res: StringResource) = androidx.compose.ui.res.stringResource(res.resourceId)
